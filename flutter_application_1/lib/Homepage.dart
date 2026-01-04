4b4eb7b1e9b00f45decd06a2afba9df726493736
Satish <dadassatish6@gmail.com>
2026-01-04 22:23:18 +0530

    added

--- flutter_application_1/lib/Homepage.dart
+++ flutter_application_1/lib/Homepage.dart
@@ -1,13 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   Homepage({super.key});
 
-  Future<List<QueryDocumentSnapshot>> get() async {
-    var firestore = FirebaseFirestore.instance.collection("gulams");
-    QuerySnapshot snapshot = await firestore.get();
-    return snapshot.docs;
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  var firestore = FirebaseFirestore.instance.collection("gulams");
+  late Future<QuerySnapshot> employeeData;
+  Future<QuerySnapshot> get() async {
+    return firestore.get();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    employeeData = get();
+  }
+
+  Future<void> deleteEmployee(String id) async {
+    await firestore.doc(id).delete();
+    setState(() {
+      employeeData = get();
+    });
+  }
+
+  Future<void> updateEmployee(String id) async {
+    await firestore.doc(id).update({"name": "kartik", "company": "Ature"});
+    setState(() {});
   }
 
   @override
@@ -15,18 +38,18 @@ class Homepage extends StatelessWidget {
     print("in build");
     return Scaffold(
       appBar: AppBar(title: Text("firestore demo")),
-      body: FutureBuilder(
-        future: get(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestore.snapshots(),
         builder: (context, snapshot) {
           print(snapshot);
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
             print("hello jatay ka ithparent");
             return Text("No data Found");
           }
-          final employess = snapshot.data!;
+          final employess = snapshot.data!.docs;
           return ListView.builder(
             itemCount: employess.length,
             itemBuilder: (context, index) {
@@ -34,6 +57,10 @@ class Homepage extends StatelessWidget {
               return ListTile(
                 title: Text(employee["name"]),
                 subtitle: Text(employee["company"]),
+                trailing: IconButton(
+                  onPressed: () => deleteEmployee(employee.id),
+                  icon: Icon(Icons.remove),
+                ),
               );
             },
           );
